9bd6baa3c5bd058090951c86d36b3c3891e5043f
Emrullah DEMİR <e177demir@gmail.com>
2022-05-25 09:13:08 +0300

    Asp.net - Caching
    Asp.NEt framework üzerinde Redis ve InMemory cache kullanımı.

---
+++ CachingSystems.InMemory/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CachingSystems.InMemory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CachingSystems.InMemory.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        private readonly IMemoryCache _memoryCache;
+
+        public HomeController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            MemoryCacheEntryOptions cacheOptions = new MemoryCacheEntryOptions();
+            cacheOptions.SetPriority(CacheItemPriority.Low);
+            cacheOptions.SetSlidingExpiration(TimeSpan.FromSeconds(10));
+            cacheOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(4));
+            cacheOptions.SetSize(20);
+           
+            _memoryCache.Set<string>("name", "emrullah",cacheOptions);                        
+            _memoryCache.Remove("name2");            
+            ViewBag.Name = _memoryCache.Get<string>("name");            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CachingSystems.Redis/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using CachingSystems.Redis.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CachingSystems.Redis.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly IDistributedCache _distributedCache;
+
+        public HomeController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+
+
+            _distributedCache.SetString("maasz", "888   1");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
